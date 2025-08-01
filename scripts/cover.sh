cf4a6f0ec8c0cafc0bbfdd3e7181fc027d265e91
jafr6 <jafr6@hotmail.com>
2025-08-01 08:12:33 -0600

    add cover.sh

---
+++ scripts/cover.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ..
+COVERAGE=$(go tool cover -func=coverage.out | grep total | awk '{print substr($3, 1, length($3)-1)}')
+ echo $COVERAGE
+COLOR="orange"
+if (( $(echo "$COVERAGE <= 50" | bc -l) )); then COLOR="red"; elif (( $(echo "$COVERAGE > 80" | bc -l) )); then COLOR="green"; fi
+echo COLOR
+curl "https://img.shields.io/badge/coverage-$COVERAGE%25-$COLOR" > images/badge.svg
